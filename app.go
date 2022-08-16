58c31b3071277f8873fb8a7433169db0b98db28d
CodapeWild <codape.wild@gmail.com>
2022-08-16 18:54:51 +0800

    compile skywalking protobuf

--- app.go
+++ app.go
@@ -1,1 +1,68 @@
 package main
+
+import (
+	"encoding/json"
+	"log"
+	"math/rand"
+	"os"
+	"strconv"
+	"time"
+)
+
+var (
+	cfg          *config
+	agentAddress = "127.0.0.1:"
+)
+
+type sender struct {
+	Threads      int `json:"threads"`
+	SendCount    int `json:"send_count"`
+	SendInterval int `json:"send_interval"`
+}
+
+type config struct {
+	DkAgent    string  `json:"dk_agent"`
+	Sender     *sender `json:"sender"`
+	Service    string  `json:"service"`
+	DumpSize   int     `json:"dump_size"`
+	RandomDump bool    `json:"random_dump"`
+	Trace      []*span `json:"trace"`
+}
+
+type tag struct {
+	Key   string      `json:"key"`
+	Value interface{} `json:"value"`
+}
+
+type span struct {
+	Resource  string  `json:"resource"`
+	Operation string  `json:"operation"`
+	SpanType  string  `json:"span_type"`
+	Duration  int64   `json:"duration"`
+	Error     string  `json:"error"`
+	Tags      []tag   `json:"tags"`
+	Children  []*span `json:"children"`
+	dumpSize  int64
+}
+
+func main() {
+
+}
+
+func init() {
+	log.SetOutput(os.Stdout)
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
+	data, err := os.ReadFile("./config.json")
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+
+	cfg = &config{}
+	if err = json.Unmarshal(data, cfg); err != nil {
+		log.Fatalln(err.Error())
+	}
+
+	rand.Seed(time.Now().UnixNano())
+	agentAddress += strconv.Itoa(30000 + rand.Intn(10000))
+}
